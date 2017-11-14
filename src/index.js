fe80de0e3b6303d3f68d992cac5ada671075a58c
Remzov <remzovnik@gmail.com>
2017-11-14 15:00:05 +0300

    Adaption 760  work

--- src/index.js
+++ src/index.js
@@ -10,3 +10,14 @@ import './sass/main.sass'
 
 /* import scripts */
 import './js/main.js'
+
+
+
+if (window.matchMedia("(max-width: 960px)").matches) {
+    let reception = document.querySelector('.header__reception');
+    let parentHeaderRight = document.querySelector('.js-header-right');
+    let contrast = document.querySelector('.js-contrast-header');
+    
+    parentHeaderRight.insertBefore(reception,contrast);
+
+}
