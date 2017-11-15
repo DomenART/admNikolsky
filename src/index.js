9180727dcdd4fc3d7b8b4c247c224047c218be19
Remzov <remzovnik@gmail.com>
2017-11-15 13:25:14 +0300

    adapting Homepage

--- src/index.js
+++ src/index.js
@@ -11,3 +11,13 @@ import './sass/main.sass'
 /* import scripts */
 import './js/main.js'
 
+
+
+// Контроль высоты uk-sticky-placeholder для блока меню
+
+if (window.matchMedia("(max-width: 960px)").matches) {
+    let sticky = document.querySelector(".uk-sticky-placeholder")
+    let menu = document.querySelector(".header-menu__container")
+
+    sticky.style.height = sticky.offsetHeight - menu.offsetHeight + "px"
+}   
