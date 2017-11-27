5c0a404c9c4868379ad45851a0ed0fece188bc80
Kundius Ruslan <kundius.ruslan@gmail.com>
2017-11-27 13:22:19 +0300

    menu

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,37 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Menu from './components/menu'
 
+document.querySelectorAll('.js-mobile-menu').forEach((el) => {
+    if (window.innerWidth > 960) return false
+
+    const menu = document.querySelector('.js-menu')
+    const parseMenu = (el) => {
+        let arr = []
+
+        for (var i = 0; i < el.children.length; i++) {
+            let link = el.children[i].querySelector('a')
+            let submenu = el.children[i].querySelector('ul')
+            let item = {
+                href: link.getAttribute('href'),
+                title: link.innerText,
+                classes: el.children[i].getAttribute('class') || ''
+            }
+
+            if (submenu)
+                item.children = parseMenu(submenu)
+
+            arr.push(item)
+        }
+
+        return arr
+    }
+
+    ReactDOM.render(
+        <Menu items={parseMenu(menu)} />,
+        el
+    )
+})
+
 
 if (window.matchMedia("(max-width: 960px)").matches) {
 
